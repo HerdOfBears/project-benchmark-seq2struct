f3a185460e8667766f01873f6aa93ed44ce46c66
jyler <jyler.menard@mail.concordia.ca>
2023-11-20 11:18:27 -0500

    forgot to re-add the most important file to the list

--- scripts/0p1_get_esmfold_files.sh
+++ scripts/0p1_get_esmfold_files.sh
@@ -4,6 +4,6 @@ mkdir ./pepstructure/esmfold_v1/
 
 huggingface_url="https://huggingface.co/facebook/esmfold_v1/resolve/main/"
 
-for file in "README.md" "config.json" "special_tokens_map.json" "tokenizer_config.json" "vocab.txt"; do
+for file in "README.md" "config.json" "pytorch_model.bin" "special_tokens_map.json" "tokenizer_config.json" "vocab.txt"; do
 	wget -P ./pepstructure/esmfold_v1/ $huggingface_url$file
 done
