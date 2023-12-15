3f0353247af0dc1b9293369dad7a70938000606d
jyler <jyler.menard@mail.concordia.ca>
2023-12-14 19:11:48 -0500

    needed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
 # Get version and release info, which is all stored in /version.py
-ver_file = os.path.join('', 'version.py')
+ver_file = os.path.join('pepstructure', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
 
