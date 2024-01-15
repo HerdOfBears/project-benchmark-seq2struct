a98ba4ebbccd2c2aec6dbebfa7149785f0cd5ee3
jyler <jyler.menard@mail.concordia.ca>
2024-01-15 14:35:42 -0500

    bash script to submit job

---
+++ scripts/run_nvt_sim_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=test_nvt_sim
+#SBATCH --account=ctb-rmansbac
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=32G
+
+# load modules/venv
+source venv/bin/activate
+module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3
+module load openmm/8.0.0
+
+# move from scripts/ to project-benchmarking/
+cd ../
+pwd
+
+python scripts/simulate_protein_in_water.py --pdb_dir outputs/esmfold/ --pdb_file starPep_44878_esmfold_prediction_pdbfixed.pdb
