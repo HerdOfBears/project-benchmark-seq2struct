66b911ff92fc48d125216938faba41234749ae1a
jyler <jyler.menard@mail.concordia.ca>
2024-01-16 15:55:38 -0500

    need to actually provide params to run_simulation() for it to have an effect

--- scripts/simulate_protein_in_water.py
+++ scripts/simulate_protein_in_water.py
@@ -195,5 +195,5 @@ if __name__=="__main__":
     ##############################
     logging.info(f"Running simulation on {pdb_file}")
     t0 = time.time()
-    run_simulation(pdb)
+    run_simulation(pdb, params=params)
     logging.info(f"Simulation took {round(time.time() - t0, 4)}s")
