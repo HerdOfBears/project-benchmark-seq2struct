1bd948a7594e04d5429a34f2a634b8d87af7bba4
jyler <jyler.menard@mail.concordia.ca>
2024-01-19 16:54:14 -0500

    test a 5aa peptide on gpu

--- scripts/run_nvt_sim_test.sh
+++ scripts/run_nvt_sim_test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=cpu_100aa
+#SBATCH --job-name=gpu_5aa
 #SBATCH --account=ctb-rmansbac
-#SBATCH --time=72:00:00
+#SBATCH --time=00:60:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=12G
 
 # move from scripts/ to project-benchmarking/
 # cd ../
@@ -17,4 +18,4 @@ module load StdEnv/2020 cuda/11.4 gcc/9.3.0 openmpi/4.0.3
 module load openmm/8.0.0
 
 
-python scripts/simulate_protein_in_water.py --pdb_dir outputs/esmfold/ --pdb_file starPep_03523_esmfold_prediction_pdbfixed.pdb --slurm_id $SLURM_JOB_ID
+python scripts/simulate_protein_in_water.py --pdb_dir outputs/esmfold/ --pdb_file starPep_09284_esmfold_prediction_pdbfixed.pdb --slurm_id $SLURM_JOB_ID
