7c6475c663a6234d2677999d59d7a28f04deac7e
jyler <jyler.menard@mail.concordia.ca>
2024-01-19 14:22:03 -0500

    reduce time request and load cuda

--- scripts/run_nvt_sim_test.sh
+++ scripts/run_nvt_sim_test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=test_nvt_sim
+#SBATCH --job-name=check_device
 #SBATCH --account=ctb-rmansbac
-#SBATCH --time=24:00:00
+#SBATCH --time=00:15:00
 #SBATCH --nodes=1
 #SBATCH --mem=32G
 
 # move from scripts/ to project-benchmarking/
-cd ../
-pwd
+# cd ../
+# pwd
 
 # load modules/venv
 source venv/bin/activate
-module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3
+module load StdEnv/2020 cuda/11.4 gcc/9.3.0 openmpi/4.0.3
 module load openmm/8.0.0
 
 
-python scripts/simulate_protein_in_water.py --pdb_dir outputs/esmfold/ --pdb_file starPep_44878_esmfold_prediction_pdbfixed.pdb
+python scripts/simulate_protein_in_water.py --pdb_dir outputs/esmfold/ --pdb_file starPep_03523_esmfold_prediction_pdbfixed.pdb
