48819c824469c66ac7c096d7fd43d14c75c86f22
jyler <jyler.menard@mail.concordia.ca>
2024-01-23 16:17:16 -0500

    gl13k 8 peptide complex sim

--- scripts/run_nvt_sim_test.sh
+++ scripts/run_nvt_sim_test.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=gpu_5aa
+#SBATCH --job-name=gpu_gl13k_8peptides
 #SBATCH --account=ctb-rmansbac
-#SBATCH --time=00:60:00
+#SBATCH --time=02:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=12G
+#SBATCH --mem=24G
 
-# move from scripts/ to project-benchmarking/
-# cd ../
-# pwd
+# Assumes running from project-benchmark-seq2struct directory
 
 # load modules/venv
 source venv/bin/activate
@@ -18,4 +16,4 @@ module load StdEnv/2020 cuda/11.4 gcc/9.3.0 openmpi/4.0.3
 module load openmm/8.0.0
 
 
-python scripts/simulate_protein_in_water.py --pdb_dir outputs/esmfold/ --pdb_file starPep_09284_esmfold_prediction_pdbfixed.pdb --slurm_id $SLURM_JOB_ID
+python scripts/simulate_protein_in_water.py --pdb_dir outputs/gl13k/ --pdb_file inputs/GL13K_8peptides_AF2.pdb --slurm_id $SLURM_JOB_ID
