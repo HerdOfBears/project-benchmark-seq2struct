067cc47d9bcf594d5861d819f210a2d3db2a23b0
jyler <jyler.menard@mail.concordia.ca>
2024-02-14 10:39:29 -0500

    debugging. test 2 nodes now

--- scripts/submit_mdruns.sh
+++ scripts/submit_mdruns.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=small_test_mdruns
 #SBATCH --account=ctb-rmansbac
 #SBATCH --time=0-00:15:00
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --gpus-per-node=4        # 6 nodes, 4 GPUs each, 24 total
 #SBATCH --mem=15G                # mem per node
 #SBATCH --mail-type=begin        # send email when job begins
@@ -25,16 +25,16 @@ scontrol show hostname > ./node_list_${SLURM_JOB_ID} # save node list for parall
 
 # "outputs/" is deep learning model outputs, but MD sim inputs
 start=`date +%s.%N` 
+# parallel -j $JOBS_PER_NODE \
+# 	--joblog $LOG_FILE \
+# 	--workdir $WDIR \
+# 	< ./scripts/gnu_cmds.txt
 parallel -j $JOBS_PER_NODE \
-	--joblog $LOG_FILE \
-	--workdir $WDIR \
-	< ./scripts/gnu_cmds.txt
-#parallel -j $JOBS_PER_NODE \
-#        --joblog parallel_test.log \
-        #--sshloginfile $WDIR/node_list_${SLURM_JOB_ID} \
-        #--sshdelay 30 \
-#        --workdir $WDIR \
-#        < ./scripts/gnu_cmds.txt
+        --joblog $LOG_FILE \
+        --sshloginfile $WDIR/node_list_${SLURM_JOB_ID} \
+        --sshdelay 30 \
+        --workdir $WDIR \
+        < ./scripts/gnu_cmds.txt
 end=`date +%s.%N`
 
 runtime=$( echo "$end - $start" | bc -l )
